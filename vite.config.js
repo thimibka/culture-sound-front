e8fb664d8078fa0d87e10feed825a27467876b74
gervais <fseetp@gmail>
2024-05-05 19:54:50 +0200

    hash esset

--- vite.config.js
+++ vite.config.js
@@ -19,23 +19,23 @@ export default defineConfig({
         theme_color: "#ffffff",
         icons: [
           {
-            src: "src/assets/16x16.png",
+            src: "src/assets/16x16.[hash].png",
             type: "image/png",
             sizes: "16x16",
             purpose: "maskable",
           },
           {
-            src: "src/assets/128x128.png",
+            src: "src/assets/128x128.[hash].png",
             type: "image/png",
             sizes: "128x128",
           },
           {
-            src: "src/assets/256x256.png",
+            src: "src/assets/256x256.[hash].png",
             type: "image/png",
             sizes: "256x256",
           },
           {
-            src: "src/assets/512x512.png",
+            src: "src/assets/512x512.[hash].png",
             type: "image/png",
             sizes: "512x512",
           },
