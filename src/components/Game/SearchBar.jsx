ec0a443da5ac92e2ac4f9704cb5ffed156cf2c56
gervais <fseetp@gmail>
2024-05-13 20:52:27 +0200

    api import

--- src/components/Game/SearchBar.jsx
+++ src/components/Game/SearchBar.jsx
@@ -8,19 +8,19 @@ export default function SearchBar({ onSearchResults, searchBarReset }) {
     const value = event.target.value;
     setSearchTerm(value);
 
-    // const response = await fetch(
-    //   `https://ws.audioscrobbler.com/2.0/?method=track.search&track=${value}&api_key=${
-    //     import.meta.env.VITE_API_LAST_FM
-    //   }&format=json`
-    // );
-
-    // const data = await response.json();
-
     const response = await fetch(
-      `https://ws.audioscrobbler.com/2.0/?method=track.search&track=${value}&api_key=05b66f4e896062dc2e236df97a3648f0&format=json`
+      `https://ws.audioscrobbler.com/2.0/?method=track.search&track=${value}&api_key=${
+        import.meta.env.VITE_API_LAST_FM
+      }&format=json`
     );
 
     const data = await response.json();
+
+    // const response = await fetch(
+    //   `https://ws.audioscrobbler.com/2.0/?method=track.search&track=${value}&api_key=05b66f4e896062dc2e236df97a3648f0&format=json`
+    // );
+
+    // const data = await response.json();
 
     if (data.results?.trackmatches?.track) {
       const results = data.results.trackmatches.track
